2b1492bcb76f2cb28a83253227c5c418df613264
jlong299 <jlong299@foxmail.com>
2016-12-19 17:20:34 +0800

    temp exmp3

--- matlab/LowLatency/exmp3_Mixed_radix_FFT.m
+++ matlab/LowLatency/exmp3_Mixed_radix_FFT.m
@@ -3,18 +3,54 @@ clear
 N_max = 1200;
 NumOfBanks = 5;
 
-NumOfFactors = 3;   % Time of iterations
+NumOfFactors = 3;   
 NumOfFactors_max = 6;
 %-------- parameters ------------
 Nf = zeros(1,NumOfFactors_max);   % factorize N
-Nf(1) = 5;
+Nf(1) = 2;
 Nf(2) = 5;
-Nf(3) = 5;
+Nf(3) = 3;
 Nf(4) = 1;
 Nf(5) = 1;
 Nf(6) = 1;
 
 N = Nf(1)*Nf(2)*Nf(3)*Nf(4)*Nf(5)*Nf(6);
+
+ena = zeros(1,NumOfFactors_max);
+for m = 1:NumOfFactors_max
+    if Nf(m)==1
+        ena(m) = 0;
+    else
+        ena(m) = 1;
+    end
+end
+
+coeff_bank = zeros(1,NumOfFactors_max-1);
+switch NumOfFactors
+    case 2
+        coeff_bank(1) = 1;
+    case 3
+        coeff_bank(1) = Nf(2);
+        coeff_bank(2) = 1;
+    case 4
+        coeff_bank(1) = Nf(2)*Nf(3);
+        coeff_bank(2) = Nf(3);
+        coeff_bank(3) = 1;
+    case 5
+        coeff_bank(1) = Nf(2)*Nf(3)*Nf(4);
+        coeff_bank(2) = Nf(3)*Nf(4);
+        coeff_bank(3) = Nf(4);
+        coeff_bank(4) = 1;
+    case 6
+        coeff_bank(1) = Nf(2)*Nf(3)*Nf(4)*Nf(5);
+        coeff_bank(2) = Nf(3)*Nf(4)*Nf(5);
+        coeff_bank(3) = Nf(4)*Nf(5);
+        coeff_bank(4) = Nf(5);
+        coeff_bank(5) = 1;
+    otherwise
+        coeff_bank(1) = 1;
+end
+
 %--------------------------------
 x_real=round((2*rand(1,N)-1)*8192);
 x_imag=round((2*rand(1,N)-1)*8192);
@@ -32,30 +68,61 @@ RAM_read = zeros(N_max/NumOfBanks,NumOfBanks);  % 5 banks
 RAM_write = zeros(N_max/NumOfBanks,NumOfBanks);  % 5 banks
 %---------------------------------
 n = zeros(1,NumOfFactors_max);
-n(1) = 0;   % 0,1,...,Nf(1)-1
-n(2) = 0;   % 0,1,...,Nf(2)-1
-n(3) = 0;   % 0,1,...,Nf(3)-1
-n(4) = 0;   % 0,1,...,Nf(4)-1
-n(5) = 0;   % 0,1,...,Nf(5)-1
-n(6) = 0;   % 0,1,...,Nf(6)-1
+% n(1) = 0;   % 0,1,...,Nf(1)-1
+% n(2) = 0;   % 0,1,...,Nf(2)-1
+% n(3) = 0;   % 0,1,...,Nf(3)-1
+% n(4) = 0;   % 0,1,...,Nf(4)-1
+% n(5) = 0;   % 0,1,...,Nf(5)-1
+% n(6) = 0;   % 0,1,...,Nf(6)-1
+
+carry_in = zeros(1,NumOfFactors_max);
+carry_in(NumOfFactors_max) = 1;
+carry_out = zeros(1,NumOfFactors_max);
 
 k = zeros(1,NumOfFactors_max);
 %----------- I/O input data to RAM ---------------
-for m=0:N-1
+for t=0:N-1
 
     switch_bank = mod( sum(n), NumOfBanks);
-    RAM_0( Nf(2)*n(1)+n(2) +1, switch_bank +1 ) = x(m +1);
+    % RAM_0( Nf(2)*n(1)+n(2) +1, switch_bank +1 ) = x(m +1);
+    RAM_0( coeff_bank(1)*n(1)+coeff_bank(2)*n(2)+coeff_bank(3)*n(3)+coeff_bank(4)*n(4)+coeff_bank(5)*n(5) +1, switch_bank +1 ) = x(t +1);
 
     % Accumulator  0 to N-1
-    if ( n(3) == Nf(3)-1 ) && ( n(2) == Nf(2)-1 ) 
-        n(1) = n(1) + 1;
-        n(2) = 0;
-        n(3) = 0;
-    elseif ( n(3) == Nf(3)-1 )
-        n(2) = n(2) + 1;
-        n(3) = 0;
-    else
-        n(3) = n(3) + 1;
+    % if ( n(3) == Nf(3)-1 ) && ( n(2) == Nf(2)-1 ) 
+    %     n(1) = n(1) + 1;
+    %     n(2) = 0;
+    %     n(3) = 0;
+    % elseif ( n(3) == Nf(3)-1 )
+    %     n(2) = n(2) + 1;
+    %     n(3) = 0;
+    % else
+    %     n(3) = n(3) + 1;
+    % end
+
+    % Accumulator  0 to N-1
+    for m = 0: NumOfFactors_max-1
+        carry_out_last = 1;
+
+        if ( ena(NumOfFactors_max-m) == 0 )
+            carry_out(NumOfFactors_max-m) = 1;
+        else
+            if ( carry_in(NumOfFactors_max-m) == 1)
+                if ( n(NumOfFactors_max-m) == Nf(NumOfFactors_max-m)-1 )
+                    carry_out(NumOfFactors_max-m) = 1;
+                    n(NumOfFactors_max-m) = 0;
+                else
+                    carry_out(NumOfFactors_max-m) = 0;
+                    n(NumOfFactors_max-m)  = n(NumOfFactors_max-m) + 1;
+                end
+            else
+                n(NumOfFactors_max-m) = n(NumOfFactors_max-m);
+                carry_out(NumOfFactors_max-m) = 0;
+            end
+        end
+        if (m < NumOfFactors_max-1)
+            carry_in(NumOfFactors_max-m-1) = carry_out_last * carry_out(NumOfFactors_max-m);
+            carry_out_last = carry_in(NumOfFactors_max-m-1); 
+        end
     end
 end
 
@@ -165,27 +232,57 @@ end
 %--------- I/O output ---------------
 Fout = zeros(N,1);
 k = zeros(1,NumOfFactors_max);
-for m=0:N-1
+carry_in = zeros(1,NumOfFactors_max);
+carry_in(1) = 1;
+carry_out = zeros(1,NumOfFactors_max);
+
+for t=0:N-1
 
     switch_bank = mod( sum(k), NumOfBanks);
 
     if (current_RAM==0)
-        Fout(m +1) = RAM_0( Nf(2)*k(1)+k(2) +1, switch_bank +1 );
+        Fout(t +1) = RAM_0( coeff_bank(1)*k(1)+coeff_bank(2)*k(2)+coeff_bank(3)*k(3)+coeff_bank(4)*k(4)+coeff_bank(5)*k(5) +1, switch_bank +1 );
     else
-        Fout(m +1) = RAM_1( Nf(2)*k(1)+k(2) +1, switch_bank +1 );
+        Fout(t +1) = RAM_1( coeff_bank(1)*k(1)+coeff_bank(2)*k(2)+coeff_bank(3)*k(3)+coeff_bank(4)*k(4)+coeff_bank(5)*k(5) +1, switch_bank +1 );
     end
 
     % Accumulator  0 to N-1
-    if ( k(1) == Nf(1)-1 ) && ( k(2) == Nf(2)-1 ) 
-        k(3) = k(3) + 1;
-        k(2) = 0;
-        k(1) = 0;
-    elseif ( k(1) == Nf(1)-1 )
-        k(2) = k(2) + 1;
-        k(1) = 0;
-    else
-        k(1) = k(1) + 1;
+    % if ( k(1) == Nf(1)-1 ) && ( k(2) == Nf(2)-1 ) 
+    %     k(3) = k(3) + 1;
+    %     k(2) = 0;
+    %     k(1) = 0;
+    % elseif ( k(1) == Nf(1)-1 )
+    %     k(2) = k(2) + 1;
+    %     k(1) = 0;
+    % else
+    %     k(1) = k(1) + 1;
+    % end
+
+    for m = 0: NumOfFactors_max-1
+        carry_out_last = 1;
+
+        if ( ena(m+1) == 0 )
+            k(m+1) = 0;
+        else
+            if ( carry_in(m+1) == 1)
+                if ( k(m+1) == Nf(m+1)-1 )
+                    carry_out(m+1) = 1;
+                    k(m+1) = 0;
+                else
+                    carry_out(m+1) = 0;
+                    k(m+1)  = k(m+1) + 1;
+                end
+            else
+                k(m+1) = k(m+1);
+                carry_out(m+1) = 0;
+            end
+        end
+        if (m < NumOfFactors_max-1)
+            carry_in(m+2) = carry_out_last * carry_out(m+1);
+            carry_out_last = carry_in(m+1); 
+        end
     end
+
 end
 
 % result
