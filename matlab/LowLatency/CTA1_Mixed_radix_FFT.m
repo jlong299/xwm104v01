f28d42a9432c0945023503bc52ef7fed037656a1
jlong299 <jlong299@foxmail.com>
2016-12-20 21:03:42 +0800

    Add comments to CTA1, almost finished

--- matlab/LowLatency/CTA1_Mixed_radix_FFT.m
+++ matlab/LowLatency/CTA1_Mixed_radix_FFT.m
@@ -1,5 +1,8 @@
 clear 
-%----------------------------------
+%-------------------------------------------------------------
+%                CTA ( )
+%   description:
+%-------------------------------------------------------------
 N_max = 1200;  % for N_max = 1536, the RAM bank size should increase to 1536/4
 NumOfBanks = 5; % total 5 RAM banks
 
@@ -23,8 +26,8 @@ for m_len = 1:100   % The end of loop body is at the end of this file
     NumOfLen = NumOfLen+1;    % total 34 different lengths   12, 24, 36, ..., 1152, 1200
 %-------- parameters ------------
 Nf = ones(1,NumOfFactors_max);   % factorize N
-% e.g. If N=1200  = 4*4*5*5*3
-%      Nf(1)=4,  Nf(2)=4,  Nf(3)=5,  Nf(4)=5, Nf(5)=3, Nf(6)=1
+% e.g. If N=1200  = 4*4*5*5*3 = N1*N2*N3*N4*N5
+%      Nf(1)=4,  Nf(2)=4,  Nf(3)=5,  Nf(4)=5, Nf(5)=3, Nf(6)=1   ( Nf(1) represents N1 ...)
 %      First must be 4,  the last must be 3  (for latency reduction)
 %------------------------------
 Nf(1) = 4;  % N1 : Fixed =4 !!!
@@ -69,33 +72,36 @@ for m = 1:NumOfFactors_max
 end
 % e.g. If N=1200  = 4*4*5*5*3,  NumOfFactors=5,  ena=[1,1,1,1,1,0]
 
+% ------------ coeff used to gen address in each bank -----------
+%   addr_in_bank = coeff_bank(2)*n(2)+coeff_bank(3)*n(3)+coeff_bank(4)*n(4)+coeff_bank(5)*n(5)+coeff_bank(6)*n(6);
+% ------------ change them to one case , not checked yet --------------
 coeff_bank = zeros(1,NumOfFactors_max);
-switch NumOfFactors
-    case 2
-        coeff_bank(2) = 1;
-    case 3
-        coeff_bank(2) = Nf(3);
-        coeff_bank(3) = 1;
-    case 4
-        coeff_bank(2) = Nf(3)*Nf(4);
-        coeff_bank(3) = Nf(4);
-        coeff_bank(4) = 1;
-    case 5
-        coeff_bank(2) = Nf(3)*Nf(4)*Nf(5);
-        coeff_bank(3) = Nf(4)*Nf(5);
-        coeff_bank(4) = Nf(5);
-        coeff_bank(5) = 1;
-    case 6
+% switch NumOfFactors
+%     case 2
+%         coeff_bank(2) = 1;
+%     case 3
+%         coeff_bank(2) = Nf(3);
+%         coeff_bank(3) = 1;
+%     case 4
+%         coeff_bank(2) = Nf(3)*Nf(4);
+%         coeff_bank(3) = Nf(4);
+%         coeff_bank(4) = 1;
+%     case 5
+%         coeff_bank(2) = Nf(3)*Nf(4)*Nf(5);
+%         coeff_bank(3) = Nf(4)*Nf(5);
+%         coeff_bank(4) = Nf(5);
+%         coeff_bank(5) = 1;
+%     case 6
         coeff_bank(2) = Nf(3)*Nf(4)*Nf(5)*Nf(6);
         coeff_bank(3) = Nf(4)*Nf(5)*Nf(6);
         coeff_bank(4) = Nf(5)*Nf(6);
         coeff_bank(5) = Nf(6);
         coeff_bank(6) = 1;
-    otherwise
-        coeff_bank(2) = 1;
-end
+%     otherwise
+%         coeff_bank(2) = 1;
+% end
 
-%--------------------------------
+%------------- gen test source data -------------------
 x_real=round((2*rand(1,N)-1)*8192);
 x_imag=round((2*rand(1,N)-1)*8192);
 
@@ -105,12 +111,16 @@ x = x_real + 1j*x_imag;
 
 FX = fft(x);
 
+%-----------  2 RAMs ping-pong,  each RAM divided into 5 banks ----------
+%-----------  currently each bank size is N_max/4  
 RAM_0 = zeros(N_max/4,NumOfBanks);  % 5 banks
 RAM_1 = zeros(N_max/4,NumOfBanks);  % 5 banks
 current_RAM = 0;
 RAM_read = zeros(N_max/4,NumOfBanks);  % 5 banks
 RAM_write = zeros(N_max/4,NumOfBanks);  % 5 banks
-%---------------------------------
+
+%------------ n1, n2, n3, n4, n5, n6 ---------------------
+%------------ n1 range 0,1,...,N1-1    n2 range 0,1,...,N2-1   .....
 n = zeros(1,NumOfFactors_max);
 
 carry_in = zeros(1,NumOfFactors_max);
@@ -118,15 +128,24 @@ carry_in(NumOfFactors_max) = 1;
 carry_out = zeros(1,NumOfFactors_max);
 
 addr_in_bank = 0;
+%----------- PART  1  ----------------------------
 %----------- I/O input data to RAM ---------------
+%----------- code considering RTL structure ------
 for t=0:N-1
 
+    % bank selection
     switch_bank = mod( sum(n), NumOfBanks);
-    % RAM_0( Nf(2)*n(1)+n(2) +1, switch_bank +1 ) = x(m +1);
+    % address in bank
     addr_in_bank = coeff_bank(2)*n(2)+coeff_bank(3)*n(3)+coeff_bank(4)*n(4)+coeff_bank(5)*n(5)+coeff_bank(6)*n(6);
     RAM_0( addr_in_bank +1, switch_bank +1 ) = x(t +1);
 
     % Accumulator  0 to N-1
+    %-------------------------------------------------------
+    % when t==0      n1=0, n2=0, ..., n6=0
+    % when t==N6-1   n1=0, n2=0, ..., n6=N6-1
+    % when t==N6     n1=0, n2=0, ..., n5=1, n6=0
+    %  ...
+    % when t==N-1    n1=N1-1, n2=N2-1, ..., n5=N5-1, n6=N6-1
     for m = 0: NumOfFactors_max-1
         carry_out_last = 1;
 
@@ -153,7 +172,9 @@ for t=0:N-1
     end
 end
 
-%----------------------------------
+%----------- PART  2  ----------------------------
+%----------- computation stages ------------------
+%----------- num of stages is NumOfFactors -------
 n=zeros(1,NumOfFactors_max);
 read_bank_index = zeros(NumOfBanks,1);
 read_data_index = zeros(NumOfBanks,1);
@@ -232,6 +253,7 @@ for m=1:NumOfFactors
         end
     end 
 
+    % update twiddle base
     tw_N = (tw_N)^(Nf(m));
 
     if (current_RAM==0)
@@ -244,7 +266,9 @@ for m=1:NumOfFactors
 
 end
 
-%--------- I/O output ---------------
+%----------- PART  3  (Inverse sequence of PART 1)----------------------------
+%----------- I/O output --------------------------
+%----------- code considering RTL structure ------
 Fout = zeros(N,1);
 k = zeros(1,NumOfFactors_max);
 carry_in = zeros(1,NumOfFactors_max);
