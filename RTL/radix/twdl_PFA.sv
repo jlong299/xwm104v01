cbfe50df8e5dc806d92df03c5b84b3c42a6f63c1
jlong299 <jlong299@foxmail.com>
2017-01-19 17:49:45 +0800

    Create twdl_PFA.sv

--- RTL/radix/twdl_PFA.sv
+++ RTL/radix/twdl_PFA.sv
@@ -19,6 +19,64 @@ module twdl_PFA #(parameter
 	output [0:4][wDataInOut-1:0]  dout_imag
 );
 
+logic [7:0]  n_tw, cnt_n_tw;
 
+always@(posedge clk)
+begin
+	if (!rst_n)
+	begin
+		n_tw <= 0;
+		cnt_n_tw <= 0;
+	end
+	else
+	begin
+		if (!in_val)
+		begin
+			n_tw <= 0;
+			cnt_n_tw <= 0;
+		end
+		else
+		begin
+			if ((n_tw==tw_ROM_exp_ceil-'d1) &&(cnt_n_tw==tw_ROM_exp_time-'d1))
+			begin
+				n_tw <= 0;
+				cnt_n_tw <= 0;
+			end
+			else if (cnt_n_tw==tw_ROM_exp_time-'d1)
+			begin
+				n_tw <= n_tw + 'd1;
+				cnt_n_tw <= 0;
+			end
+			else
+			begin
+				n_tw <= n_tw;
+				cnt_n_tw <= cnt_n_tw + 'd1;
+			end
+		end
+	end
+end
+
+genvar i;
+
+assign wraddr[0] = 0;
+generate
+for (i=3'd0; i<3'd5; i=i+3'd1)
+	assign wraddr[i] = tw_ROM_addr_step*n_tw*(i-3'd1);
+endgenerate
+
+generate
+for (i=0; i<5; i++) begin : twdl_ROM
+mrd_ROM_fake 
+mrd_ROM_fake_0(
+	.clk  (clk),
+
+	.factor  (factor),
+	.wraddr  (wraddr[i]),
+
+	.dout_real  (tw_real[i]),
+	.dout_imag  (tw_imag[i])
+);
+end
+endgenerate
 
 endmodule
