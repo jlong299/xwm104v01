a04b6b54e8e855b5609ba9b6a6860802435332fd
jlong299 <jlong299@foxmail.com>
2017-02-03 11:50:12 +0800

    Divider 44/12,  not tested

--- RTL/twiddle/divider_pipe0.sv
+++ RTL/twiddle/divider_pipe0.sv
@@ -15,4 +15,48 @@ module divider_pipe0  #(parameter
 	
 );
 
+localparam w_pipe = 4;   // Each pipeline handles 4 bits width
+localparam n_stages = (w_divident-w_divisor)/w_pipe; // Num of stages
+
+logic [w_divident-1 : 0]  remd_quot_r [0 : n_stages];  
+wire  [w_pipe-1:0]  quot [0 : n_stages-1];
+wire  [w_divisor-1:0]  remd [0 : n_stages-1];
+
+genvar i;
+generate
+	for (i=0; i < n_stages; i++) begin
+		divider_type2 #(
+			.w_divident (w_divisor+w_pipe), //16
+			.w_divisor (w_divisor) //12
+			)
+			(
+			.dividend (remd_quot_r[i]
+				       [w_divident-1 : w_divident-w_divisor-w_pipe]),  
+			.divisor (divisor),
+
+			.quotient (quot[i]),
+			.remainder (remd[i])
+		);
+	end
+endgenerate
+
+assign remd_quot_r[0] = dividend; 
+integer j;
+always@(posedge clk)
+begin
+	if (!rst_n)
+		for (j=1; j <= n_stages; j++) begin
+			remd_quot_r[j] <= 0;
+		end
+	else begin
+		for (j=1; j <= n_stages; j++) begin
+			remd_quot_r[j][w_divident-1:w_divident-w_divisor] <= remd[j-1];
+			remd_quot_r[j][w_pipe-1:0] <= quot[j-1];
+			remd_quot_r[j][w_divident-w_divisor-1:w_pipe] <=
+			           remd_quot_r[j-1][w_divident-w_divisor-1-w_pipe:0];
+		end
+	end
+end
+
+
 endmodule
--- RTL/twiddle/divider_type2.sv
+++ RTL/twiddle/divider_type2.sv
@@ -12,28 +12,34 @@ module divider_type2 #(parameter
 	output reg [w_divisor-1:0] remainder
 );
 
+localparam w_pipe = 1;   // Each pipeline handles 1 bits width
+localparam n_stages = (w_divident-w_divisor)/w_pipe; // Num of stages
 
-// reg [3:0] 	dividend_t [8:0];
-// reg [3:0] 	remainder_t [8:0];
+logic [w_divident-1 : 0]  remd_quot_r [0 : n_stages];  
+logic  [w_pipe-1:0]  quot [0 : n_stages-1];
+logic  [w_divisor-1:0]  remd [0 : n_stages-1];
+logic  [w_divisor:0]  subtr [0 : n_stages-1];
 
-// assign quotient[11:9] = 3'b000;
+genvar i;
+generate
+	for (i=0; i < n_stages; i++) begin
+		assign subtr[i] = remd_quot_r[i]
+		            [w_divident-1 : w_divident-w_divisor-w_pipe] - divisor;
+		assign quot[i] = ~(subtr[i][w_divisor]);
+		assign remd[i] = (subtr[i][w_divisor])? remd_quot_r[i]
+		            [w_divident-2 : w_divident-w_divisor-w_pipe] : 
+		            subtr[i][w_divisor-1:0] ;
+	end
+endgenerate
 
-// assign dividend_t[8] = dividend[11:8];
-
-// genvar i;
-// generate 
-// 	for ( i = 0; i < 8; i++) begin
-// 		assign remainder_t[8-i] = dividend_t[8-i] - 4'd7;
-// 		assign quotient[8-i] = (remainder_t[8-i][3])? 1'b0 : 1'b1;
-// 		assign dividend_t[8-i-1] =(remainder_t[8-i][3]) ? 
-// 		       { dividend_t[8-i][2:0], dividend[8-i-1] } :
-// 		       { remainder_t[8-i][2:0], dividend[8-i-1] };
-// 	end
-// endgenerate
-
-// assign remainder_t[0] = dividend_t[0][3:0] - 4'd7;
-// assign quotient[0] = (remainder_t[0][3])? 1'b0 : 1'b1;
-
-// assign remainder =(remainder_t[0][3])? dividend_t[0][2:0] : remainder_t[0];
+assign remd_quot_r[0] = dividend; 
+generate
+for (i=1; i <= n_stages; i++) begin
+	assign remd_quot_r[j][w_divident-1:w_divident-w_divisor] = remd[j-1];
+	assign remd_quot_r[j][w_pipe-1:0] = quot[j-1];
+	assign remd_quot_r[j][w_divident-w_divisor-1:w_pipe] =
+	           remd_quot_r[j-1][w_divident-w_divisor-1-w_pipe:0];
+end
+endgenerate
 
 endmodule
