ea3e7434ec0bb19d2c5dc61d8c31cfdb69582f4a
jlong299 <jlong299@foxmail.com>
2017-04-11 14:29:56 +0800

    Temp

--- RTL/twiddle/coeff_twdl_CTA.sv
+++ RTL/twiddle/coeff_twdl_CTA.sv
@@ -15,10 +15,10 @@ module coeff_twdl_CTA #(parameter
   	input [20-1:0] twdl_quotient,
   	input [12-1:0] twdl_remainder,
 
-	output signed [wDataOut-1:0] dout_real_1,
-	output signed [wDataOut-1:0] dout_imag_1,
-	output signed [wDataOut-1:0] dout_real_3,
-	output signed [wDataOut-1:0] dout_imag_3
+	output logic signed [wDataOut-1:0] dout_real_1,
+	output logic signed [wDataOut-1:0] dout_imag_1,
+	output logic signed [wDataOut-1:0] dout_real_3,
+	output logic signed [wDataOut-1:0] dout_imag_3
 );
 
 // logic [31:0] quotient, quotient_round;
@@ -117,9 +117,30 @@ always@(posedge clk) begin
 end
 
 
-  // Generate table of atan values
-  logic signed [20-1:0] atan_table [0:30];
+  // // Generate table of atan values
+  // logic signed [20-1:0] atan_table [0:30];
  
+  // assign atan_table[00] = 'b00100000000000000000; // 45.000 degrees -> atan(2^0)
+  // assign atan_table[01] = 'b00010010111001000000; // 26.565 degrees -> atan(2^-1)
+  // assign atan_table[02] = 'b00001001111110110011; // 14.036 degrees -> atan(2^-2)
+  // assign atan_table[03] = 'b00000101000100010001; // atan(2^-3)
+  // assign atan_table[04] = 'b00000010100010110000;
+  // assign atan_table[05] = 'b00000001010001011101;
+  // assign atan_table[06] = 'b00000000101000101111;
+  // assign atan_table[07] = 'b00000000010100010111;
+  // assign atan_table[08] = 'b00000000001010001011;
+  // assign atan_table[09] = 'b00000000000101000101;
+  // assign atan_table[10] = 'b00000000000010100010;
+  // assign atan_table[11] = 'b00000000000001010001;
+  // assign atan_table[12] = 'b00000000000000101000;
+  // assign atan_table[13] = 'b00000000000000010100;
+  // assign atan_table[14] = 'b00000000000000001010;
+  // assign atan_table[15] = 'b00000000000000000101;
+  // assign atan_table[16] = 'b00000000000000000010;
+  // assign atan_table[17] = 'b00000000000000000001;
+
+  logic signed [20-1:0] atan_table [0:13];
+
   assign atan_table[00] = 'b00100000000000000000; // 45.000 degrees -> atan(2^0)
   assign atan_table[01] = 'b00010010111001000000; // 26.565 degrees -> atan(2^-1)
   assign atan_table[02] = 'b00001001111110110011; // 14.036 degrees -> atan(2^-2)
@@ -134,20 +155,17 @@ end
   assign atan_table[11] = 'b00000000000001010001;
   assign atan_table[12] = 'b00000000000000101000;
   assign atan_table[13] = 'b00000000000000010100;
-  assign atan_table[14] = 'b00000000000000001010;
-  assign atan_table[15] = 'b00000000000000000101;
-  assign atan_table[16] = 'b00000000000000000010;
-  assign atan_table[17] = 'b00000000000000000001;
 
-CORDIC
+
+CORDIC_v02
 cordic_inst(clk, cosine[0], sine[0], xin, 16'd0, quotient_r[0], atan_table);
 
-CORDIC
+CORDIC_v02
 cordic_inst2(clk, cosine[2], sine[2], xin, 16'd0, quotient_r[2], atan_table);
 
-assign dout_real_1= cosine[0];
-assign dout_imag_1 = - sine[0];
-assign dout_real_3 = cosine[2];
-assign dout_imag_3 = - sine[2];
+always@(posedge clk) dout_real_1 <= cosine[0];
+always@(posedge clk) dout_imag_1 <= - sine[0];
+always@(posedge clk) dout_real_3 <= cosine[2];
+always@(posedge clk) dout_imag_3 <= - sine[2];
 
 endmodule
